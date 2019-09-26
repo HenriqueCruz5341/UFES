7b5a6b58642fa74fe2ba28cbb73a065e5ecf1a97
Henrique Paulino Cruz <henriquepaulinocruz@hotmail.com>
2019-09-26 09:06:44 -0300

    Adicionando mais exercicios

--- TEP_I/TADs/BigInt_04/bigint_04.c
+++ TEP_I/TADs/BigInt_04/bigint_04.c
@@ -9,7 +9,7 @@ void LeNumero(tIntGrande *n) {
     scanf("%s", n->intGrande);
 }
 
-int MaisDigitos(tIntGrande num1, tIntGrande num2){
+int MaisDigitos(tIntGrande num1, tIntGrande num2) {
     if (strlen(num1.intGrande) > strlen(num2.intGrande)) {
         return 1;
     } else if (strlen(num1.intGrande) < strlen(num2.intGrande)) {
@@ -70,11 +70,11 @@ void SomarMaisUm(tIntGrande *n) {
             n->intGrande[j] = n->intGrande[k];
         }
         n->intGrande[0] = '1';
-        n->intGrande[tamanho+1] = '\0';
+        n->intGrande[tamanho + 1] = '\0';
     }
 }
 
-void TirarUm(tIntGrande *n){
+void TirarUm(tIntGrande *n) {
     int tamanho = strlen(n->intGrande), i, j, k, flag = 1;
 
     if (n->intGrande[tamanho - 1] != '0') {
@@ -93,11 +93,11 @@ void TirarUm(tIntGrande *n){
         }
     }
     if (n->intGrande[0] == '0' && tamanho != 1) {
-        for (j = tamanho-2, k = tamanho - 1; j >= 0; j--, k--) {
+        for (j = tamanho - 2, k = tamanho - 1; j >= 0; j--, k--) {
             n->intGrande[j] = n->intGrande[k];
         }
         n->intGrande[0] = '9';
-        n->intGrande[tamanho-1] = '\0';
+        n->intGrande[tamanho - 1] = '\0';
     }
 }
 
@@ -105,25 +105,76 @@ void ImprimirIntGrande(tIntGrande n) {
     printf("%s", n.intGrande);
 }
 
-tIntGrande SomarIntGrande(tIntGrande num1, tIntGrande num2){
-    tIntGrande soma;
-    int menorTamanho, i, maiorTamanho;
+void IgualaQtdDigitos(tIntGrande *n1, tIntGrande *n2){
+    int i, diferenca;
+    char string[101];
     
-    if (MaisDigitos(num1, num2) == 1) {
-        menorTamanho = strlen(num2.intGrande);
+    if(MaisDigitos(*n1, *n2) == 1){
+        diferenca = strlen(n1->intGrande) - strlen(n2->intGrande);
+        for(i = 0; i < diferenca; i++){
+            string[i] = '0';
+        }
+        string[diferenca] = '\0';
+        strcat(string, n2->intGrande);
+        strcpy(n2->intGrande, string);
+    }else if(MaisDigitos(*n1, *n2) == 2){
+        diferenca = strlen(n2->intGrande) - strlen(n1->intGrande);
+        for(i = 0; i < diferenca; i++){
+            string[i] = '0';
+        }
+        string[diferenca] = '\0';
+        strcat(string, n1->intGrande);
+        strcpy(n1->intGrande, string);
+    }
+}
+
+tIntGrande SomarIntGrande(tIntGrande num1, tIntGrande num2) {
+    tIntGrande soma, somador;
+    int i, maiorTamanho, j, k, flag = 0;
+    char aux, aux1;
+
+    if (MaisDigitos(num1, num2) == 1 || MaisDigitos(num1, num2) == 0) {
+        soma = num1;
+        somador = num2;
         maiorTamanho = strlen(num1.intGrande);
     } else if (MaisDigitos(num1, num2) == 2) {
-        menorTamanho = strlen(num1.intGrande);
-        maiorTamanho = strlen(num2.intGrande);
-    }else{
-        menorTamanho = strlen(num1.intGrande);
+        soma = num2;
+        somador = num1;
         maiorTamanho = strlen(num2.intGrande);
     }
     
-    for(i = menorTamanho-1; i >= 0; i--){
-        soma.intGrande[i] = ((num1.intGrande[i] - 48) + (num2.intGrande[i] - 48)) + 48;
+    IgualaQtdDigitos(&soma, &somador);
+
+    for (i = maiorTamanho - 1; i >= 0; i--) {
+        aux = ((soma.intGrande[i] - 48) + (somador.intGrande[i] - 48));
+        if (aux > 9) {
+            aux1 = aux;
+            j = i;
+            while (aux1 > 9) {
+                soma.intGrande[j] = (aux1 % 10) + 48;
+                j--;
+                if (j < 0) {
+                    flag++;
+                    break;
+                } else {
+                    aux1 = (soma.intGrande[j] - 48) + 1;
+                }
+            }
+            if(!flag){
+                soma.intGrande[j] = (aux1 + 48);
+            }
+        } else {
+            soma.intGrande[i] = aux + 48;
+        }
     }
-    soma.intGrande[maiorTamanho] = '\0';
+
+    if (flag) {
+        for (j = maiorTamanho, k = maiorTamanho - 1; k >= 0; j--, k--) {
+            soma.intGrande[j] = soma.intGrande[k];
+        }
+        soma.intGrande[0] = '1';
+        soma.intGrande[maiorTamanho + 1] = '\0';
+    }
     
     return soma;
 }
@@ -139,4 +190,3 @@ int main() {
 
     return 0;
 }
-
